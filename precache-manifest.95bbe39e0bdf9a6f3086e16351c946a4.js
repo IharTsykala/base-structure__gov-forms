c9aeca19bff7f2bfef8047e35db0f87b3ea85a69
Your Name <Sudja09@bk.ru>
2021-01-25 15:53:30 +0300

    Updates

---
+++ precache-manifest.95bbe39e0bdf9a6f3086e16351c946a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73303d054d1aa2ebb1a4def4d57a8c74",
+    "url": "/base-structure__gov-forms/index.html"
+  },
+  {
+    "revision": "1731c1465f99835b6495",
+    "url": "/base-structure__gov-forms/static/css/2.0d383db3.chunk.css"
+  },
+  {
+    "revision": "9b276f7411ca74c08903",
+    "url": "/base-structure__gov-forms/static/css/main.7ad46bdb.chunk.css"
+  },
+  {
+    "revision": "1731c1465f99835b6495",
+    "url": "/base-structure__gov-forms/static/js/2.eab1b666.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/base-structure__gov-forms/static/js/2.eab1b666.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b276f7411ca74c08903",
+    "url": "/base-structure__gov-forms/static/js/main.90f38fe8.chunk.js"
+  },
+  {
+    "revision": "b11ba071f91cc2248db9",
+    "url": "/base-structure__gov-forms/static/js/runtime-main.bde3746c.js"
+  }
+]);
